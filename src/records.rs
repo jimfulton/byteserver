fce6f053ca636c2abfc7ee32a8429719dfc1e759
Jim Fulton <jim@jimfulton.info>
2021-11-28 14:51:27 -0700

    don't * use util

--- src/records.rs
+++ src/records.rs
@@ -1,6 +1,9 @@
+use std::io::prelude::*;
+
+use byteorder::{ByteOrder, BigEndian, ReadBytesExt, WriteBytesExt};
+
 use crate::index;
 use crate::util;
-use crate::util::*;
 
 pub static HEADER_MARKER: &'static [u8] = b"fs2 ";
 
@@ -20,23 +23,23 @@ impl FileHeader {
         where T: std::io::Read + std::io::Seek
     {
         util::check_magic(&mut reader, HEADER_MARKER);
-        io_assert(reader.read_u64::<BigEndian>()? == 4096,
+        util::io_assert(reader.read_u64::<BigEndian>()? == 4096,
                   "Bad header length")?;
         let alignment = reader.read_u64::<BigEndian>()?;
-        let h = match String::from_utf8(read_sized16(&mut reader)?) {
+        let h = match String::from_utf8(util::read_sized16(&mut reader)?) {
             Ok(previous) =>
                 FileHeader { alignment: alignment, previous: previous },
-            _ => return Err(io_error("Bad previous utf8")),
+            _ => return Err(util::io_error("Bad previous utf8")),
         };
-        io_assert(reader.seek(io::SeekFrom::Start(4088))? == 4088,
+        util::io_assert(reader.seek(std::io::SeekFrom::Start(4088))? == 4088,
                   "Seek failed")?;
-        io_assert(reader.read_u64::<BigEndian>()? == 4096,
+        util::io_assert(reader.read_u64::<BigEndian>()? == 4096,
                   "Bad header extra length")?;
         Ok(h)
     }
 
-    pub fn write<T>(&self, writer: &mut T) -> io::Result<()>
-        where T: io::Write + io::Seek
+    pub fn write<T>(&self, writer: &mut T) -> std::io::Result<()>
+        where T: std::io::Write + std::io::Seek
     {
         writer.write_all(&HEADER_MARKER)?;
         writer.write_u64::<BigEndian>(4096)?;
@@ -45,8 +48,8 @@ impl FileHeader {
         if self.previous.len() > 0 {
             writer.write_all(&self.previous.clone().into_bytes())?;
         }
-        io_assert(
-            writer.seek(io::SeekFrom::Start(4088))? == 4088,
+        util::io_assert(
+            writer.seek(std::io::SeekFrom::Start(4088))? == 4088,
             "seek failed"
         )?;
         writer.write_u64::<BigEndian>(4096)?;
@@ -57,7 +60,7 @@ impl FileHeader {
 #[derive(PartialEq, Debug)]
 pub struct TransactionHeader {
     pub length: u64,
-    pub id: Tid,
+    pub id: util::Tid,
     pub ndata: u32,
     pub luser: u16,
     pub ldesc: u16,
@@ -67,15 +70,15 @@ pub const TRANSACTION_HEADER_LENGTH: u64 = 28;
 
 impl TransactionHeader {
 
-    fn new(tid: Tid) -> TransactionHeader {
+    fn new(tid: util::Tid) -> TransactionHeader {
         TransactionHeader {
             length: 0, id: tid, luser: 0, ldesc: 0, lext: 0, ndata: 0 }
     }
 
-    pub fn read(mut reader: &mut dyn io::Read)
-                -> io::Result<TransactionHeader> {
+    pub fn read(mut reader: &mut dyn std::io::Read)
+                -> std::io::Result<TransactionHeader> {
         let length = reader.read_u64::<BigEndian>()?;
-        let mut h = TransactionHeader::new(read8(&mut reader)?);
+        let mut h = TransactionHeader::new(util::read8(&mut reader)?);
         h.length = length;
         h.ndata = reader.read_u32::<BigEndian>()?;
         h.luser = reader.read_u16::<BigEndian>()?;
@@ -85,24 +88,24 @@ impl TransactionHeader {
     }
 
     pub fn update_index<T>(&self, mut reader: &mut T, index: &mut index::Index,
-                           mut last_oid: Oid)
-                           -> io::Result<Oid>
-        where T: io::Read + io::Seek {
+                           mut last_oid: util::Oid)
+                           -> std::io::Result<util::Oid>
+        where T: std::io::Read + std::io::Seek {
         let mut pos =
             reader.seek(
-                io::SeekFrom::Current(
+                std::io::SeekFrom::Current(
                     self.luser as i64 + self.ldesc as i64 + self.lext as i64))?;
 
         for i in 0 .. self.ndata {
             let ldata = reader.read_u32::<BigEndian>()?;
-            let oid = read8(&mut reader)?;
+            let oid = util::read8(&mut reader)?;
             index.insert(oid, pos);
             if oid > last_oid {
                 last_oid = oid;
             }
             pos += DATA_HEADER_SIZE + ldata as u64;
             if i + 1 < self.ndata {
-                seek(&mut reader, pos)?;
+                util::seek(&mut reader, pos)?;
             }
         }
 
@@ -114,8 +117,8 @@ impl TransactionHeader {
 #[derive(PartialEq, Debug)]
 pub struct DataHeader {
     pub length: u32,
-    pub id: Oid,
-    pub tid: Tid,
+    pub id: util::Oid,
+    pub tid: util::Tid,
     pub previous: u64,
     pub offset: u64,
 }
@@ -125,19 +128,19 @@ pub const DATA_PREVIOUS_OFFSET: u64 = 20;
 
 impl DataHeader {
 
-    fn new(tid: Tid) -> TransactionHeader {
+    fn new(tid: util::Tid) -> TransactionHeader {
         TransactionHeader {
             length: 0, id: tid, luser: 0, ldesc: 0, lext: 0, ndata: 0 }
     }
 
-    pub fn read(reader: &mut dyn io::Read) -> io::Result<DataHeader> {
+    pub fn read(reader: &mut dyn std::io::Read) -> std::io::Result<DataHeader> {
         // assume reader is unbuffered
         let mut buf = [0u8; DATA_HEADER_SIZE as usize];
         reader.read_exact(&mut buf)?;
         Ok(DataHeader {
             length: BigEndian::read_u32(&buf[0..4]),
-            id: read8(&mut &buf[4..])?,
-            tid: read8(&mut &buf[12..])?,
+            id: util::read8(&mut &buf[4..])?,
+            tid: util::read8(&mut &buf[12..])?,
             previous: BigEndian::read_u64(&buf[20..]),
             offset: BigEndian::read_u64(&buf[28..]),
         })
@@ -168,12 +171,12 @@ mod tests {
 
     #[test]
     fn read_file_header() {
-        let mut reader = io::Cursor::new(file_header_sample(b""));
+        let mut reader = std::io::Cursor::new(file_header_sample(b""));
         let h = FileHeader::read(&mut reader).unwrap();
         assert_eq!(&h.previous, "");
         assert_eq!(h.alignment, 1<<30);
 
-        let mut reader = io::Cursor::new(file_header_sample(b"previous"));
+        let mut reader = std::io::Cursor::new(file_header_sample(b"previous"));
         let h = FileHeader::read(&mut reader).unwrap();
         assert_eq!(h.previous, "previous");
         assert_eq!(h.alignment, 1<<30);
@@ -182,7 +185,7 @@ mod tests {
     #[test]
     fn write_file_header() {
         
-        let mut writer = io::Cursor::new(vec![0u8; 0]);
+        let mut writer = std::io::Cursor::new(vec![0u8; 0]);
         let h = FileHeader {
             previous: String::new(),
             alignment: 1<<30,
@@ -190,7 +193,7 @@ mod tests {
         h.write(&mut writer).unwrap();
         assert_eq!(writer.into_inner(), file_header_sample(b""));
         
-        let mut writer = io::Cursor::new(vec![0u8; 0]);
+        let mut writer = std::io::Cursor::new(vec![0u8; 0]);
         let h = FileHeader {
             previous: String::from("previous"),
             alignment: 1<<30,
@@ -204,22 +207,22 @@ mod tests {
         // Note that the transaction-header read method is called
         // after reading the record marker.
 
-        let mut cursor = io::Cursor::new(Vec::new());
+        let mut cursor = std::io::Cursor::new(Vec::new());
 
         // Write out some sample data:
-        write_u64(&mut cursor, 9999).unwrap();
-        cursor.write_all(&p64(1234567890)).unwrap();
-        write_u32(&mut cursor, 2).unwrap();
-        write_u16(&mut cursor, 11).unwrap();
-        write_u16(&mut cursor, 22).unwrap();
-        write_u32(&mut cursor, 33).unwrap();
-        seek(&mut cursor, 0).unwrap();
+        util::write_u64(&mut cursor, 9999).unwrap();
+        cursor.write_all(&util::p64(1234567890)).unwrap();
+        util::write_u32(&mut cursor, 2).unwrap();
+        util::write_u16(&mut cursor, 11).unwrap();
+        util::write_u16(&mut cursor, 22).unwrap();
+        util::write_u32(&mut cursor, 33).unwrap();
+        util::seek(&mut cursor, 0).unwrap();
 
         let h = TransactionHeader::read(&mut cursor).unwrap();
         assert_eq!(
             h,
             TransactionHeader {
-                length: 9999, id: p64(1234567890), ndata: 2,
+                length: 9999, id: util::p64(1234567890), ndata: 2,
                 luser: 11, ldesc: 22, lext: 33,
             });
     }
